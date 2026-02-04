da22a5dfeab90bcd7b2f37a09fb3e0f88f635f10
karleljonte <karlnjuguna@gmail.com>
2026-02-04 23:51:24 +0300

    added new files

---
+++ assingment1.py
@@ -0,0 +1,5 @@
+wheight = 78
+height = 1.75
+height_square = height ** 2
+bmi = (wheight / height_square)
+print("bmi is: ", bmi)
---
+++ assinmgent1b.py
@@ -0,0 +1,6 @@
+length = 48
+width = 25
+area = length * width
+perimeter = ((length * 2) + (width * 2) )
+print("the perimeter is: ", perimeter )
+print("the area is: ", area)
