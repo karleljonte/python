16ce46b162831c39352209d333ca79a1169e97fa
karleljonte <karlnjuguna@gmail.com>
2026-02-04 13:59:21 +0300

    added new python files

---
+++ lesson1c.py
@@ -0,0 +1,45 @@
+# Arithmetic operations
+# Addition
+
+number1 = 20
+number2 = 10
+addition = number1 + number2
+print("The sum of the two numbers is: ", addition)
+
+
+# substraction
+number3 = 50
+number4 = 15
+substraction= number3 - number4
+print("the difference of the numbers is: ", substraction)
+
+# multiplicatin
+number5 = 10
+number6 = 6
+multiplication = number6 * number5
+print("the multiplication is: ", multiplication)
+
+
+# Division
+number7 = 21
+number8 = 7
+division = number7 / number8
+print("The division of the numbers is: ", division)
+
+# modulus - Rhis is the remeinder u have after dividing two numbers
+
+number9 = 14
+number10 = 5
+modulus= number9 % number10
+print("The modulus of the numbers is: ", modulus)
+
+
+# Floor Divisions - This is the number of times a number subdivides another ignoring the reminder
+floor = number9 // number10
+print("The floor division of the two numbers is: ", floor)
+
+# exponent (**)
+# power of
+number11 = 10
+exponent = number11 ** 2
+print("exponent is: ", exponent)
