a17fcad46d83ea8b5ba4f8f3c0175a8d4efd3005
karleljonte <karlnjuguna@gmail.com>
2026-02-04 13:06:59 +0300

    added python files

---
+++ lasseon1a.py
@@ -0,0 +1,29 @@
+# What is a variable: Variables r labkes which as containers to hold values. In python, a variable is alays associated with some values.
+# We use th assingment operator (=) to assing values to a variable.
+# Anything inside of quotes in python is called a string
+message = "Hello there! Hope everythimg is fine"
+print(message)
+
+
+message2 = "Good Bye!"
+print (message2)
+
+
+# concastinating variable (joining two or more outputs together)
+message3 = message + message2
+
+print(message3)
+
+# variables can be named using a letter,  number and an underscore. They can start with a letter and an underscore but not with a numner
+
+name_1 = "Karl"
+print(name_1)
+
+full_name = "Karl Njuguna"
+print(full_name)
+
+total_values = 1000
+print(total_values)
+
+# Variables names are not supposed to be key words/ reseved characteris in python
+#print(for)
---
+++ test.py
@@ -0,0 +1,7 @@
+print("hello karl")
+
+#commetn
+
+""""this is a comment"""
+
+'''also a comment but called a docstring'''
