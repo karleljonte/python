cdb418f55f579a657b5e2fd2cdc5d0337f0e7bd3
karleljonte <karlnjuguna@gmail.com>
2026-02-04 14:14:08 +0300

    added new lines

---
+++ lesson1d.py
@@ -0,0 +1,15 @@
+# Creat a python program that is able to calculate the simple interest given the principle as 50000, rate as 5% and time as 8 years. SI -= (p*R*t)/100
+principle = 50000
+time = 8
+rate = 5 / 100
+
+answer_1 = (principle * time * rate) / 100
+
+print("the simple interest is: ", answer_1)
+
+# Assingment
+# 1. Write a python program that is able to calculate the BMI of aperson whose weight is 78kgs and height is 1.75 meters. BMI = (wight) / (height squared)
+
+# 2. Find the area and perimeter of a triangle whose length is 48cm and width is 25cm
+
+# Research on python list, tuple and Dictionary Data types. 
